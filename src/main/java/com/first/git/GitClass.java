057258ddb142e8810ef4dca64830cec35e37bc14
MrZeng <2603477634@qq.com>
2021-07-02 15:20:09 +0800

    hot-fox second commit

--- src/main/java/com/first/git/GitClass.java
+++ src/main/java/com/first/git/GitClass.java
@@ -5,6 +5,7 @@ public class GitClass {
         System.out.println("hello,git");
         System.out.println("hello,git1");
         System.out.println("hello,git2");
+        System.out.println("host-test");
 
     }
 }
